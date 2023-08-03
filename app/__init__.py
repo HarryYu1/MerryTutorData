f43132f15b0d2b871193e95f61677be407f8f012
HarryYu1 <hyu01@stu.naperville203.org>
2023-08-02 21:45:28 -0500

    Added Login Manager Auth Module

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,25 @@
 from flask import Flask
-
+import flask
+from . import secretdata
+import flask_login
 
 def create_app():
 
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "mysupersecretkey"
+    app.config['SECRET_KEY'] = secretdata.SECRET_KEY
+    app.secret_key = secretdata.SECRET_KEY
+
+    @app.route("/", methods = ["GET", "POST"])
+    def redirect_login():
+        return flask.redirect("/login")
 
     #blueprints here
+    from .auth_module import auth
+    app.register_blueprint(auth.authbp)
 
     from .attendance_module import attendance
     app.register_blueprint(attendance.attendancebp)
 
-    from .auth_module import auth
-    app.register_blueprint(auth.authbp)
+
 
     return app
