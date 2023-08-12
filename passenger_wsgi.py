67d76a05173948f08efedb3e130b1d19e57d8573
HarryYu1 <hyu01@stu.naperville203.org>
2023-08-12 13:07:14 -0500

    Add interpreter path for dreamhost

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,15 @@
 from app import app as application
 
+import sys, os
+# INTERP = os.path.join(os.environ['HOME'], 'example.com', 'venv', 'bin', 'python3')
+INTERP = os.path.expanduser("~/venv/bin/python3")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+#sys.path.append(os.getcwd())
+#sys.path.append('~/merrytutordata.site/app')
+
+
 if __name__ == '__main__':
     app_instance = application
     app_instance.run()
