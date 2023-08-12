3e6abd05f98c1e4faa280e79a0a914f31ce0822e
HarryYu1 <hyu01@stu.naperville203.org>
2023-08-12 16:26:29 -0500

    Quick fix to app error

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
-from app import app as application
+import app
 
 #sys.path.append(os.getcwd())
 #sys.path.append('~/merrytutordata.site/app')
-app_instance = application
+app_instance = app.create_app()
 
 if __name__ == '__main__':
     app_instance.run()
